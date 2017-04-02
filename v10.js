65c1cef77b54f3dc1b66bda0d94c0fdfa8cbbfff
Dangkey <hillisnillig@gmail.com>
2017-04-02 19:11:21 +0000

    Add files via upload

---
+++ v10.js
@@ -0,0 +1,19 @@
+(function() {    
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, {
+    tags: "mount rainier",
+    tagmode: "any",
+    format: "json"
+  })
+    .done(function( data ) {
+      $.each( data.items, function( i, item ) {        
+        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#draggable" );        
+        if ( i === 3 ) {
+          return false;
+        }
+      });
+    });
+})();
+$( function() {
+    $( "#draggable" ).draggable({ revert: true });
+  } );
