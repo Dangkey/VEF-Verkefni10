ebfd26aabf0559e1f9d5b5258956f58634c32fe5
Dangkey <hillisnillig@gmail.com>
2017-04-02 20:53:54 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # VEF-Verkefni10
-Verkefni 10 í Vef
+Vinstra megin á síðunni eru myndir sem er hægt að draga upp og niður sem koma í gegnum flickr api með getJson
+Hægra megin er Google Maps sem sýnir staðinn sem er á myndunum
--- v10.js
+++ v10.js
@@ -1,19 +1,32 @@
 (function() {    
   var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
   $.getJSON( flickerAPI, {
-    tags: "mount rainier",
+    tags: "Havasu Falls",
     tagmode: "any",
     format: "json"
   })
     .done(function( data ) {
       $.each( data.items, function( i, item ) {        
         $( "<img>" ).attr( "src", item.media.m ).appendTo( "#draggable" );        
-        if ( i === 3 ) {
+        if ( i === 10 ) {
           return false;
         }
       });
     });
 })();
+
+
 $( function() {
-    $( "#draggable" ).draggable({ revert: true });
+    $( "#draggable" ).draggable({ revert: true,axis: "y",appendTo: "body",scroll:false });
   } );
+
+
+function myMap() {
+  var mapCanvas = document.getElementById("googleMap");
+  var mapOptions = {
+    center: new google.maps.LatLng(36.255322, -112.698316),
+    zoom:20,
+    mapTypeId:google.maps.MapTypeId.HYBRID
+  };
+  var map = new google.maps.Map(mapCanvas,mapOptions);
+}
